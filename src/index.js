3ab5905e47167bb7551baf13def849631c26eef4
Kyle-Laptop <67805222+wolfSkullCave@users.noreply.github.com>
2026-04-09 18:32:12 +0200

    add weather data fetching and widget functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./styles.css";
+import { addWidget, fetchWeatherData, getWeather } from "./weather-api";
 
-console.log("Hello Webpack Template!");
+const key = "WHNN7SGBSX9BJL6W9RJ9AGVEV";
+let location = "Cape Town, South Africa";
+
+fetchWeatherData(key, location).then((weatherData) => {
+  console.log("Test:", weatherData);
+});
+
+document.getElementById("location-btn").addEventListener("click", (event) => {
+  event.preventDefault();
+  const location = document.getElementById("location-input").value;
+  addWidget(key, location);
+  fetchWeatherData(key, location).then((data) => {
+    days(data);
+  });
+});
+
+function days(data) {
+  console.log("days:", data.days);
+}
---
+++ src/weather-api.js
@@ -0,0 +1,39 @@
+export async function fetchWeatherData(key, location) {
+  let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}?unitGroup=metric&key=${key}&contentType=json`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(`Current weather in ${location}:`, data.currentConditions);
+    return data;
+  } catch (e) {
+    console.error("Error fetching weather data:", e);
+  }
+}
+
+
+export function addWidget(key, location) {
+  window.weatherWidgetConfig = window.weatherWidgetConfig || [];
+  window.weatherWidgetConfig.push({
+    selector: ".weatherWidget",
+    apiKey: key, //Sign up for your personal key
+    location: location, //Enter an address
+    unitGroup: "metric", //"us" or "metric"
+    forecastDays: 5, //how many days forecast to show
+    title: location, //optional title to show in the
+    showTitle: true,
+    showConditions: true,
+  });
+
+  (function () {
+    var d = document,
+      s = d.createElement("script");
+    s.src =
+      "https://www.visualcrossing.com/widgets/forecast-simple/weather-forecast-widget-simple.js";
+    s.setAttribute("data-timestamp", +new Date());
+    (d.head || d.body).appendChild(s);
+  })();
+}
